f9989b7335191dc541add27c366b568e4efc5fcb
dilara153232@gmail.com <dilara153232@gmail.com>
2021-05-16 18:22:41 +0300

    Add project files.

---
+++ NetCoreMvc/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreMvc.Models.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMvc.Controllers
+{
+    public class ProductController : Controller
+    {
+        Context c = new Context();
+
+        public IActionResult Index()
+        {
+            var values = c.Products.ToList();
+            return View(values);
+        }
+    }
+}
---
+++ NetCoreMvc/Models/Classes/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreMvc.Models.Classes
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server =(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection = true");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
